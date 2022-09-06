eade9e475e5bbdb19c23e8c7e33ad742bf0bac76
soundaryasg <soundaryas71995@gmail.com>
2022-09-06 15:49:46 +0530

    sound's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -2,4 +2,12 @@ package org.test;
 
 public class Sample {
 
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Test 4");
+	System.out.println("Test 5");
+	System.out.println("Test 6");
+
+}
 }
